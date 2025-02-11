8885f514af54668504f2fb8a2ae5a7327d2bc1d9
andresdanielmtz <103913163+andresdanielmtz@users.noreply.github.com>
2025-02-10 20:07:58 -0600

    Add: Basic Routing integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
+import Home from "./pages/Home.js";
+import {Link, Route, Routes} from "react-router-dom";
+import Search from "./pages/Search.js";
+import Dashboard from "./pages/Dashboard.js";
+import Profile from "./pages/Profile.js";
 
 function App() {
-
-  return (
-      <>
-          <h6 className="text-yellow-400"> This is an example </h6>
-          <h6 className="text-yellow-400"> This is an example </h6>
-
-      </>
-  )
+    return (
+        <>
+            <nav>
+                <Link to="/"> Home </Link>
+                <Link to="/search"> Search </Link>
+                <Link to="/dashboard"> Dashboard </Link>
+                <Link to="/profile"> Profile </Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/search" element={<Search/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App
