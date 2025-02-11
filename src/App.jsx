74b12b58cc8c519e7566982e0fdaf4f82059133a
andresdanielmtz <103913163+andresdanielmtz@users.noreply.github.com>
2025-02-10 19:18:20 -0600

    Add: Tailwind Integration into Vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-        <h6> This is an example </h6>
-    </>
+      <>
+          <h6 className="text-yellow-400"> This is an example </h6>
+          <h6 className="text-yellow-400"> This is an example </h6>
+
+      </>
   )
 }
 
