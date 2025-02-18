35dc9c88b4f408da5bd2f1de38c13f5ebb24c5d2
Andrés Martínez <autummata@gmail.com>
2025-02-17 18:20:16 -0600

    Fix: Dotenv variables

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,37 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import MovieCard from '../components/MovieCard';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MovieCard from "../components/MovieCard";
+import React from "react";
 
 interface Movie {
-    id: number;
-    title: string;
-    poster_path: string;
-    vote_average: number;
+  id: number;
+  title: string;
+  poster_path: string;
+  vote_average: number;
 }
 
 export default function Home() {
-    const [movies, setMovies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    const API_KEY = "";
-    const API_URL = `${API_KEY}`;
-    
-    useEffect(() => {
-        axios.get(API_URL).then((response) => {
-            setMovies(response.data.results);
-        });
-    }, []);
+  const TMDB_API_KEY = import.meta.env.VITE_MDB_API_KEY;
+  const TMDB_API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${TMDB_API_KEY}`;
 
+  useEffect(() => {
+    axios.get(TMDB_API_URL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
 
-    return (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
-            {movies.map((movie) => (
-                <MovieCard
-                    key={movie.id}
-                    title={movie.title}
-                    poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    rating={movie.vote_average}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          rating={movie.vote_average}
+        />
+      ))}
+    </div>
+  );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+/// <reference types="vite/client" />
+/// <reference types="vite/types/importMeta.d.ts" />
+
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
@@ -8,7 +11,4 @@ config();
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    define: {
-        'process.env': process.env
-    }
 })
